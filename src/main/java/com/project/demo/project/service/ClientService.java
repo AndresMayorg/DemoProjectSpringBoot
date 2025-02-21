9d04f2175cd4df2c48efeca95e013b735ef217ac
Andres <ahdresmayorga@gmail.com>
2025-02-21 11:33:52 -0500

    create methods post,put and delete clients

--- src/main/java/com/project/demo/project/service/ClientService.java
+++ src/main/java/com/project/demo/project/service/ClientService.java
@@ -3,21 +3,44 @@ package com.project.demo.project.service;
 import com.project.demo.project.persistence.entity.ClientEntity;
 import com.project.demo.project.persistence.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
 
 import java.util.List;
 
 @Service
 public class ClientService {
+    private final JdbcTemplate jdbcTemplate;
     private final ClientRepository clientRepository;
 
     @Autowired
-    public ClientService(ClientRepository clientRepository) {
+    public ClientService(JdbcTemplate jdbcTemplate, ClientRepository clientRepository) {
+        this.jdbcTemplate = jdbcTemplate;
         this.clientRepository = clientRepository;
     }
 
     public List<ClientEntity> getAllClients() {
-        return this.clientRepository.findAll();
+        //return this.clientRepository.findAll();
+        return this.jdbcTemplate.query("SELECT * FROM clients", new BeanPropertyRowMapper<>(ClientEntity.class));
     }
 
+    public ClientEntity getClientById(int idClient) {
+        //return this.clientRepository.findById(idClient).orElse(null);
+        return this.jdbcTemplate.queryForObject("SELECT * FROM clients WHERE id_client" +
+                " = ?", new Object[]{idClient}, new BeanPropertyRowMapper<>(ClientEntity.class));
+    }
+
+    public ClientEntity saveClient(ClientEntity client) {
+        return this.clientRepository.save(client);
+    }
+
+    public boolean existsClient(int idClient) {
+        return this.clientRepository.existsById(idClient);
+    }
+
+    public void deleteClient(int idClient) {
+        this.clientRepository.deleteById(idClient);
+    }
 }
--- src/main/java/com/project/demo/project/web/controller/ClientController.java
+++ src/main/java/com/project/demo/project/web/controller/ClientController.java
@@ -3,10 +3,8 @@ package com.project.demo.project.web.controller;
 import com.project.demo.project.persistence.entity.ClientEntity;
 import com.project.demo.project.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -24,4 +22,38 @@ public class ClientController {
     public ResponseEntity<List<ClientEntity>> getAllClients() {
         return ResponseEntity.ok(this.clientService.getAllClients());
     }
+
+    @GetMapping("/{idClient}")
+    public ResponseEntity<ClientEntity> getClientById(@PathVariable int idClient) {
+        return ResponseEntity.ok(this.clientService.getClientById(idClient));
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientEntity> saveClient(@RequestBody ClientEntity client) {
+        if (client.getIdClient() == null || !this.clientService.existsClient(client.getIdClient())) {
+            return ResponseEntity.ok(this.clientService.saveClient(client));
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<ClientEntity> updateClient(@RequestBody ClientEntity client) {
+        if (client.getIdClient() != null && this.clientService.existsClient(client.getIdClient())) {
+            return ResponseEntity.ok(this.clientService.saveClient(client));
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @DeleteMapping("/{idClient}")
+    public ResponseEntity<Void> deleteClient(@PathVariable int idClient) {
+        if (this.clientService.existsClient(idClient)) {
+            this.clientService.deleteClient(idClient);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
 }
